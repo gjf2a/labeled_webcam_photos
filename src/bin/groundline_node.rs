092c791a0a14efc4b5575498b28a1b97a5de3af7
Gabriel Ferrer <ferrer@hendrix.edu>
2026-09-16 16:37:22 -0500

    Restored FPS - let's try only updating when we get new images.

--- src/bin/groundline_node.rs
+++ src/bin/groundline_node.rs
@@ -9,7 +9,7 @@ use nokhwa::{
 use pancurses::{Input, endwin, initscr, noecho};
 use r2r::{Context, Node, Publisher, QosProfile, std_msgs::msg::String as Ros2String};
 use smol::lock::Mutex;
-use std::sync::Arc;
+use std::{sync::Arc, time::Instant};
 
 const PERIOD: u64 = 100;
 
@@ -41,13 +41,18 @@ fn curses_loop(robot_name: &str) -> anyhow::Result<()> {
     window.nodelay(true);
     noecho();
 
+    let start = Instant::now();
+    let mut frames = 0;
     loop {
+        let fps = frames as f64 / start.elapsed().as_secs_f64();
         let (wrows, wcols) = window.get_max_yx();
         let header =
-            format!("Type `q` to exit\nterminal rows: {wrows} cols: {wcols}\n");
-        if let Some(image) = image.try_lock() {
-            if let Some(image) = image.as_ref() {
+            format!("Type `q` to exit\nterminal rows: {wrows} cols: {wcols}\n{fps:.2} fps;\n");
+        if let Some(mut image_buffer) = image.try_lock() {
+            if let Some(image) = image_buffer.as_ref() {
+                frames += 1;
                 menu.show_in_terminal(&window, header.as_str(), &image, false);
+                *image_buffer = None;
             }
         }
 
